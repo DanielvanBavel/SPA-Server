f65093221c03b1399c7238b965015d43c21fb030
Daniel van Bavel <dvanbavel@gmail.com>
2017-12-14 10:13:44 +0100

    delete unnessery routes

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,12 +1,8 @@
 import express = require('express');
-import userRoutes from './user.routes';
-import postRoutes from './post.routes';
 import groupRoutes from './groups.routes';
 
 const router = express.Router();
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
 router.use('/groups', groupRoutes);
 
 export default router;
--- src/model/schemas/post.schema.ts
+++ src/model/schemas/post.schema.ts
@@ -1,7 +1,5 @@
 import { Document, Schema } from 'mongoose';
 import { ICommentDocument, CommentSchema } from './comment.schema';
-import { IUserDocument, UserSchema} from './user.schema';
-
 export interface IPostDocument extends Document { 
     user_id: string;
     username: string;
